1bb83601e2061ed146a1cab0f6b8012606e1bee4
Todd Holford <toddholford1996@gmail.com>
2024-12-11 01:59:00 -0600

    - separated content columns into their own components

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -38,6 +38,8 @@ import { getUserUUID } from "../../services/UsersService";
 import { CalculatorIcon } from "@heroicons/react/24/outline";
 import { Calculator } from "../../components/Calculator";
 import {Helmet} from "react-helmet-async";
+import {FormColumn} from "../../components/FormColumn";
+import {CalendarColumn} from "../../components/CalendarColumn";
 
 export const DashboardPage = () => {
   const [date, setDate] = useState(
@@ -68,7 +70,6 @@ export const DashboardPage = () => {
   const [paymentExpenseAmount, setPaymentExpenseAmount] = useState(0);
   const [expenseDueDate, setExpenseDueDate] = useState(0);
 
-  const notify = () => toast("Saved calculation values");
 
   //get payments
   useEffect(() => {
@@ -148,35 +149,6 @@ export const DashboardPage = () => {
     );
   }, [selectedDate]);
 
-  const savePaycheckInfo = () => {
-    getIds(setIds, setFetchError);
-    if (ids.includes(userUUID)) {
-      console.log("save paycheck info - try update");
-      updatePaycheckInfo(
-        userUUID,
-        incomeAmount,
-        payFrequency,
-        repeatingExpenses,
-        fetchError,
-        setIncomeAmount,
-        setPayFrequency,
-        setFetchError,
-      );
-    } else {
-      console.log("save paycheck info - try add");
-      addPaycheckInfo(
-        userUUID,
-        incomeAmount,
-        payFrequency,
-        repeatingExpenses,
-        fetchError,
-        setIncomeAmount,
-        setPayFrequency,
-        setFetchError,
-      );
-    }
-    notify();
-  };
 
   const handlePaymentSubmit = (e) => {
     e.preventDefault();
@@ -197,11 +169,6 @@ export const DashboardPage = () => {
     );
   };
 
-  const toggleCalculator = () => {
-    setCalculatorOpen(!calculatorOpen);
-    console.log("open calculator after : ", calculatorOpen);
-  };
-
   return (
     <>
       {calculatorOpen ? (
@@ -213,173 +180,29 @@ export const DashboardPage = () => {
           id="main-content"
           className="md:gap-2 md:col-span-23 md:row-span-full md:grid md:grid-cols-12 md:grid-rows-4 bg-gray-950"
         >
-          <article
-            id="left"
-            className="col-span-3 row-span-full grid grid-flow-row grid-rows-24 bg-gray-900"
-          >
-            <article
-              id="a"
-              className="col-span-1 row-span-1 flex flex-row items-center justify-between bg-gray-900 px-2 text-gray-400 outline outline-1 outline-offset-0 outline-gray-700"
-            >
-              <div className="">Remainder</div>
-              <div
-                className="cursor-pointer text-emerald-700 hover:text-emerald-500 active:text-emerald-300"
-                onMouseDown={toggleCalculator}
-              >
-                <CalculatorIcon className="h-6 w-6" />
-              </div>
-            </article>
-            <article
-              id="remainder"
-              className="col-span-1 row-span-2 content-center bg-gray-850 text-center outline outline-1 outline-offset-0 outline-gray-700"
-            >
-              <p className="bg-clip-text text-transparent bg-gradient-to-r from-emerald-700 to-emerald-200 text-4xl font-bold">
-                {incomeAmount
-                  ? `${"$" + (incomeAmount - (expenseAmount + repeatingExpenseTotal)).toFixed(2)}`
-                  : `${"$" + "0.00"}`}
-              </p>
-            </article>
-            <article
-              id="c"
-              className="col-span-1 content-center bg-gray-900 px-2 text-gray-400 outline outline-1 outline-offset-0 outline-gray-700"
-            >
-              Calculate Remainder
-            </article>
-            <article
-              id="d"
-              className="col-span-1 row-span-5 bg-gray-900 px-16 py-4 text-center outline outline-1 outline-offset-0 outline-gray-700"
-            >
-              <CustomNumberInput
-                id="current_avg_income"
-                inputValue={incomeAmount}
-                setInputValue={setIncomeAmount}
-                numberType="decimal"
-                adjustBy="100"
-                placeholder="Current Average Income"
-                customClassNames="pb-2"
-              ></CustomNumberInput>
-              <CustomDropdown
-                options={payFrequencies}
-                selected={payFrequency}
-                onSelect={(value) => setPayFrequency(value)}
-                placeholder="Select Pay Period"
-                customClassNames="pb-2"
-              />
-              <CustomDateInput
-                startDate={startDate}
-                setDate={setDate}
-                customClassNames=""
-              />
-              <button
-                type="button"
-                onMouseDown={savePaycheckInfo}
-                className="mt-4 h-8 w-4/12 rounded-sm bg-emerald-950 text-center text-sm outline outline-1 outline-offset-0 outline-emerald-700 hover:bg-emerald-900 hover:outline-emerald-600 active:bg-emerald-800"
-              >
-                Save
-              </button>
-              <ToastContainer
-                toastClassName="bg-emerald-950 text-center rounded-sm text-sm outline outline-1 outline-offset-0 outline-emerald-700 hover:bg-emerald-900 hover:outline-emerald-600 active:bg-emerald-800"
-                icon={false}
-                position="bottom-left"
-                autoClose={5000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-                theme="dark"
-              />
-            </article>
-            <article
-              id="e"
-              className="col-span-1 row-span-1 flex flex-row items-center justify-between bg-gray-900 px-2 text-gray-400 outline outline-1 outline-offset-0 outline-gray-700"
-            >
-              <div className="">Repeating Expenses</div>
-              <div className="">{`$${repeatingExpenseTotal.toFixed(2)}`}</div>
-            </article>
-            <article
-              id="d"
-              className="col-span-1 row-span-14 max-h-108 overflow-y-scroll bg-gray-900 px-14 py-4 text-center outline outline-1 outline-offset-0 outline-gray-700"
-            >
-              <RepeatingExpenses
-                userUUID={userUUID}
-                repeatingExpenses={repeatingExpenses}
-                setRepeatingExpenses={setRepeatingExpenses}
-                repeatingExpenseTotal={repeatingExpenseTotal}
-                setRepeatingExpenseTotal={setRepeatingExpenseTotal}
-                setFetchError={setFetchError}
-              />
-            </article>
-          </article>
-          <article
-            id="center"
-            className="col-span-4 row-span-full grid grid-flow-row grid-rows-24 bg-gray-900 outline outline-1 outline-offset-0 outline-gray-700"
-          >
-            <article
-                id="remaining_expenses_total"
-                className="col-span-1 row-span-1 flex flex-row content-center items-center justify-between bg-gray-900 px-2 text-gray-400 outline outline-1 outline-offset-0 outline-gray-700"
-            >
-              <div className="">Remaining Expenses Total</div>
-              <div className="">{
-                `${selectedDate} - ${payweekCalendarEndDate}`
-              }</div>
-            </article>
-            <article
-                id="remainder"
-                className="col-span-1 row-span-2 content-center bg-gray-850 text-center outline outline-1 outline-offset-0 outline-gray-700"
-            >
-              <p className="bg-clip-text text-transparent bg-gradient-to-r from-rose-700 to-rose-200 text-4xl font-bold">
-                {incomeAmount
-                    ? `${"$" + (selectedExpenseAmount).toFixed(2)}`
-                    : `${"$" + "0.00"}`}
-              </p>
-            </article>
-            <article
-              id="e"
-              className="col-span-1 row-span-1 flex flex-row items-center justify-between bg-gray-900 px-2 text-gray-400 outline outline-1 outline-offset-0 outline-gray-700"
-            >
-              <div className="">Current Pay Period</div>
-              <div className="">{`$${(expenseAmount).toFixed(2)}`}</div>
-            </article>
-            <article
-              id="f"
-              className="col-span-1 row-span-5 content-center bg-gray-900 px-2 outline outline-1 outline-offset-0 outline-gray-700"
-            >
-              <PaycheckCalendar
-                date={date}
-                calendarRows={payweekCalendarRows}
-                selectedDate={selectedDate}
-                setSelectedDate={setSelectedDate}
-              />
-            </article>
-            <article
-              id="selected_date_expenses"
-              className="col-span-1 row-span-1 flex flex-row content-center items-center justify-between bg-gray-900 px-2 text-gray-400 outline outline-1 outline-offset-0 outline-gray-700"
-            >
-              <div className="">Selected Date Expenses</div>
-              <div className="">{`$${getSelectedDateExpenseTotal(selectedDateExpenses)}`}</div>
-            </article>
-            <article
-              id="h"
-              className="col-span-1 row-span-14 max-h-108 overflow-y-scroll content-start bg-gray-900 px-4 py-4 text-gray-400 outline outline-1 outline-offset-0 outline-gray-700"
-            >
-              {selectedDate ? (
-                  <div className="pb-2 text-gray-600">Payments on the {AddDueDateSuffix(selectedDate)}</div>
-              ) : (
-                  <></>
-              )}
-              {selectedDateExpenses.map((dateExpense) => (
-                  <PaycheckCalendarPaymentInfo
-                      key={dateExpense.id}
-                      weekday={dateExpense.expense_due_date}
-                      amount={dateExpense.expense_amount}
-                      expense={dateExpense.expense_name}
-                  />
-              ))}
-            </article>
-          </article>
+          <FormColumn
+              userUUID={userUUID}
+              repeatingExpenses={repeatingExpenses}
+              setRepeatingExpenses={setRepeatingExpenses}
+              setRepeatingExpenseTotal={setRepeatingExpenseTotal}
+              startDate={startDate}
+              setDate={setDate}
+              calculatorOpen={calculatorOpen}
+              setCalculatorOpen={setCalculatorOpen}
+              incomeAmount={incomeAmount}
+              setIncomeAmount={setIncomeAmount}
+              expenseAmount={expenseAmount}
+              repeatingExpenseTotal={repeatingExpenseTotal}
+              payFrequency={payFrequency}
+              setPayFrequency={setPayFrequency}
+              payFrequencies={payFrequencies}
+              ids={ids}
+              setIds={setIds}
+              fetchError={fetchError}
+              setFetchError={setFetchError}
+          />
+
+          {/*<CalendarColumn incomeAmount={incomeAmount} payweekCalendarEndDate={payweekCalendarEndDate} date={date} payweekCalendarRows={payweekCalendarRows} selectedDate={selectedDate} setSelectedDate={setSelectedDate} selectedDateExpenses={selectedDateExpenses} setSelectedDateExpenses={setSelectedDateExpenses} expenseAmount={expenseAmount} paycheckFrequency={paycheckFrequency} paycheckCalculations={paycheckCalculations} userUUID={userUUID} ids={ids} fetchError={fetchError} setFetchError={setFetchError} selectedExpenseAmount={selectedExpenseAmount} setSelectedExpenseAmount={setSelectedExpenseAmount} remainingAmount={remainingAmount} setRemainingAmount={setRemainingAmount} calculatorOpen={calculatorOpen} setCalculatorOpen={setCalculatorOpen}/>*/}
           <article
             id="right"
             className="col-span-5 row-span-full grid grid-flow-row grid-rows-24 bg-gray-900 outline outline-1 outline-offset-0 outline-gray-700"
